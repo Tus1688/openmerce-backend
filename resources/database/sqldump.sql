452a05afc7aaa96513d5e345c41d07a225b33de0
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2023-04-15 18:18:42 +0700

    feat(resources): oltp, olap, dfd schema

--- resources/database/sqldump.sql
+++ resources/database/sqldump.sql
@@ -1,14 +1,13 @@
-CREATE TABLE
-    customers(
-        id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-        email VARCHAR(72) UNIQUE NOT NULL,
-        hashed_password BINARY(60) NOT NULL,
-        phone_number VARCHAR(15) UNIQUE,
-        first_name VARCHAR(50) NOT NULL,
-        last_name VARCHAR(50) NOT NULL,
-        birth_date DATETIME,
-        gender VARCHAR(6)
-    );
+CREATE TABLE customers(
+    id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+    email VARCHAR(72) UNIQUE NOT NULL,
+    hashed_password BINARY(60) NOT NULL,
+    phone_number VARCHAR(15) UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    birth_date DATETIME,
+    gender VARCHAR(6)
+);
 
 CREATE TABLE blacklist_domains (
     domain_name VARCHAR(255),
